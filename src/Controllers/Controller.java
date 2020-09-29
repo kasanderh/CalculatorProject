28db560a273128be7c2d19d57f3dcd4671b68d45
Kasander Hanssen <kasanderh@gmail.com>
2020-09-29 08:36:30 +0200

    Initial commit

---
+++ src/Controllers/Controller.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+
+public class Controller {
+
+    @FXML
+    private TextArea resultTextArea;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9
+            ,buttonPlus, buttonMinus, buttonEquals, buttonSqRt, buttonSquared, buttonPlusMinus, buttonMultiplied, buttonDivided
+            ,buttonDot, buttonClear, buttonPercent;
+
+
+
+}
