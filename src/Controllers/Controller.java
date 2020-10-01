02a3fdd67805c992ae05d027a1139f6f0260161d
Kasander Hanssen <kasanderh@gmail.com>
2020-10-01 08:34:28 +0200

    Added handleButtonClicked onAction to the number buttons Added operations() method

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -39,51 +39,20 @@ public class Controller {
         int numberClicked = Integer.parseInt(buttonClicked.getText());
         if(numberClicked > (-1) && numberClicked < 10) {
             resultTextArea.appendText(buttonClicked.getText());
-            System.out.println(buttonClicked.getText() + "was clicked!");
+            System.out.println(buttonClicked.getText() + " was clicked!");
         } else {
             System.out.println("Not a number clicked");
         }
 
-//        switch (buttonClicked.getId()) {
-//            case "button0":
-//                resultTextArea.appendText("0");
-//                break;
-//            case "button1":
-//                resultTextArea.appendText("1");
-//                break;
-//            case "button2":
-//                resultTextArea.appendText("2");
-//                break;
-//            case "button3":
-//                resultTextArea.appendText("3");
-//                break;
-//            case "button4":
-//                resultTextArea.appendText("4");
-//                break;
-//            case "button5":
-//                resultTextArea.appendText("5");
-//                break;
-//            case "button6":
-//                resultTextArea.appendText("6");
-//                break;
-//            case "button7":
-//                resultTextArea.appendText("7");
-//                break;
-//            case "button8":
-//                resultTextArea.appendText("8");
-//                break;
-//            case "button9":
-//                resultTextArea.appendText("9");
-//                break;
-//        }
     }
 
-    //Eventhandler operations buttons
+    @FXML
+    public void operations(Event event) {
+        if(resultTextArea.equals("") || resultTextArea == null) {
+            resultLabel.setText("Please enter a number first.");
+        } else {
 
-    //Method change sign textarea * (-1)
-
-
-
-
+        }
+    }
 
 }
