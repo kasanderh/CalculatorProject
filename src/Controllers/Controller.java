657dc6e5421fdcf5f75c210743fcb67e9b348090
Kasander Hanssen <kasanderh@gmail.com>
2020-10-12 08:29:37 +0200

    Fixed operation() so operator can only be selected once. Added code to use resultLabel Cleaned up commented out code

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -21,64 +21,27 @@ public class Controller {
             ,buttonPlus, buttonMinus, buttonEquals, buttonSqRt, buttonSquared, buttonPlusMinus, buttonMultiplied, buttonDivided
             ,buttonDot, buttonClear, buttonPercent;
 
-    private String currentText;
 
     private boolean start = true;
     private Calculation calculation = new Calculation(0,0,"");
 
 
-//    @FXML
-//    public void handleNumberButtons(Event event) {
-//        Button buttonClicked = (Button) event.getSource();
-////        double numberClicked = Double.parseDouble(buttonClicked.getText());
-//        int numberClicked = Integer.parseInt(buttonClicked.getText());
-//        if(start) {
-//            String inputNumbers = "";
-//            if(calculation.getOperator().equals("")) {
-//                if(calculation.getNumber1() == 0) {
-//                    inputNumbers = inputNumbers + buttonClicked.getText();
-//                                        System.out.println("Num1 is currently: " + "nada" + " and new number: " + inputNumbers);
-//                    calculation.setNumber1((double)Double.parseDouble(inputNumbers));
-//                    resultTextArea.setText(String.valueOf(inputNumbers));
-//                } else {
-//                    double currentNumbers = calculation.getNumber1();
-//                    inputNumbers = inputNumbers + buttonClicked.getText();
-//                    String newNumbers = currentNumbers + inputNumbers;
-//                    System.out.println("Num1 is currently: " + currentNumbers + " and new number: " + inputNumbers + " and together: " + newNumbers);
-//                    calculation.setNumber1((double) Double.parseDouble(newNumbers));
-//                    resultTextArea.setText(String.valueOf(newNumbers));
-//                }
-//            }
-////            System.out.println("Number 1: " + numberClicked + " was selected!");
-////            start = false;
-//        } else {
-//            calculation.setNumber2(numberClicked);
-//            System.out.println("Number 2: " + numberClicked + " was selected!");
-//            resultTextArea.appendText(String.valueOf(numberClicked));
-//        }
-//    }
-
     @FXML
     public void handleNumberButtons(Event event) {
         Button buttonClicked = (Button) event.getSource();
-        int numberClicked = Integer.parseInt(buttonClicked.getText());
-//        String inputNumber1 = "";
-//        String inputNumber2 = "";
+        resultLabel.setText("");
         if (start) {
+            // Code for num1.
             if (calculation.getNumber1() == 0) {
                 int inputNumber1 = Integer.parseInt(buttonClicked.getText());
                 System.out.println("Input number 1 is now: " + inputNumber1);
                 calculation.setNumber1(inputNumber1);
                 outputResult(buttonClicked.getText());
-                // or setOutput(String.valueOf(inputNumber1));
-                // code for num1
-                // num2 is not relevant here
             } else {
-                // here the numberedEntered is not the first digit.
+                // Here the numberedEntered is not the first digit in num1.
                 int currentNumber = calculation.getNumber1();
                 System.out.println("Old number 1: " + currentNumber);
                 int enteredInt = Integer.parseInt(buttonClicked.getText());
-                //int newNumber = currentNumber + Integer.parseInt(buttonClicked.getText());
                 String newNumber = currentNumber + buttonClicked.getText();
                 System.out.println("New Number1: " +newNumber);
                 int intNewNumber = Integer.parseInt(newNumber);
@@ -87,21 +50,15 @@ public class Controller {
             }
 
         } else {
-            // not start, so operator != "";, and num1 is selected
+            // Not start, so operator != "";, and num1 is selected. Code for num2
             if (calculation.getNumber2() == 0) {
                 int inputNumber2 = Integer.parseInt(buttonClicked.getText());
                 System.out.println("Input number 2 is now: " + inputNumber2);
                 calculation.setNumber2(inputNumber2);
                 outputResult(getCurrentOutput() + buttonClicked.getText());
-//                String inputNumber2 = buttonClicked.getText();
-//                System.out.println("Input number 2 is now: " + inputNumber2);
-//                // test: Sout "Number2 is now:" + num2
-//                calculation.setNumber2(Integer.parseInt(inputNumber2));
-//                outputResult(inputNumber2);
-                // or setOutput(String.valueOf(inputNumber2));
 
             } else {
-                // here the numberedEntered is not the first digit.
+                // Here the numberedEntered is not the first digit in num2.
                 int currentNumber = calculation.getNumber2();
                 System.out.println("Old number 2: " + currentNumber);
                 String newNumber = currentNumber + buttonClicked.getText();
@@ -110,12 +67,7 @@ public class Controller {
                 calculation.setNumber2(intNewNumber);
                 String newOutput = getCurrentOutput() + buttonClicked.getText();
                 outputResult(newOutput);
-//                int currentNumber2 = (int) calculation.getNumber2();
-//                int newNumber2 = currentNumber2 + Integer.parseInt(buttonClicked.getText());
-//                int enteredDouble2 = Integer.parseInt(buttonClicked.getText());
-//                calculation.setNumber2(currentNumber2 + enteredDouble2);
-//                outputResult(Integer.toString(newNumber2));
-                                //code for num2
+
             }
         }
     }
@@ -124,26 +76,41 @@ public class Controller {
     @FXML
     public void operations(Event event) {
         String operator = ((Button) event.getSource()).getText();
-        resultTextArea.appendText(" " + operator + " ");
-        if (operator.equals("=")) {
+        if (calculation.getOperator().equals("")) {
+            resultTextArea.appendText(" " + operator + " ");
+//            if (operator.equals("=")) {
+//                System.out.println("Equals = was clicked");
+//                String result = String.valueOf(calculation.calculation(calculation.getNumber1(), calculation.getNumber2(), calculation.getOperator()));
+//                resultTextArea.setText(result);
+//                start = true;
+//                calculation.setOperator("");
+//            } else {
+                System.out.println("Operator set to: " + operator);
+                calculation.setOperator(operator);
+                start = false;
+//            }
+        } else if (operator.equals("=")) {
             System.out.println("Equals = was clicked");
             String result = String.valueOf(calculation.calculation(calculation.getNumber1(), calculation.getNumber2(), calculation.getOperator()));
             resultTextArea.setText(result);
+            resultLabel.setText("Result: ");
             start = true;
             calculation.setOperator("");
+
         } else {
-            System.out.println("Operator set to: " +operator);
-            calculation.setOperator(operator);
-            start = false;
+            System.out.println("Operator already chosen.");
+            resultLabel.setText("Operator already chosen");
         }
     }
 
     @FXML
-    public void clearWindow(Event event) {
+    public void clearWindow() {
         resultTextArea.setText("");
+        resultLabel.setText("");
         calculation.setNumber1(0);
         calculation.setNumber2(0);
         calculation.setOperator(null);
+        System.out.println("Clear!");
         start = true;
     }
 
